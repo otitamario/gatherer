f55bc33808f3096a9b63145f07c465ac615f5ac2
Mario S J <mario2013sj@gmail.com>
2022-11-16 16:18:50 -0400

    some Rspec tests

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,71 @@
+require "rails_helper"
+
+RSpec.describe Project do
+  
+  describe "completion" do
+    let(:project) { Project.new }
+    let(:task) { Task.new }
+
+    it "considers a project with no tasks to be done" do
+      expect(project).to be_done
+    end
+
+    it "knows that a project with an incomplete task is not done" do
+      project.tasks << task
+      expect(project).not_to be_done
+    end
+
+    it "marks a project done if its tasks are done" do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
+  end
+
+## START: estimates
+  describe "estimates" do
+
+    # #START: new_setup
+    let(:project) { Project.new }
+    let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
+    let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) }
+    let(:small_not_done) { Task.new(size: 1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before(:example) do
+      project.tasks = [newly_done, old_done, small_not_done, large_not_done]
+    end
+    # #END:  new_setup
+
+    it "can calculate total size" do
+      expect(project.total_size).to eq(10)
+    end
+    it "can calculate remaining size" do
+      expect(project.remaining_size).to eq(5)
+    end
+
+    it "knows its velocity" do
+        expect(project.completed_velocity).to eq(3)
+      end
+      
+      it "knows its rate" do
+        expect(project.current_rate).to eq(1.0 / 7) # <label id="code.algebra" />
+      end
+      
+      it "knows its projected days remaining" do
+        expect(project.projected_days_remaining).to eq(35) # <label id="code.number" />
+      end
+      
+      it "knows if it is not on schedule" do
+        project.due_date = 1.week.from_now
+        expect(project).not_to be_on_schedule
+      end
+      
+      it "knows if it is on schedule" do
+        project.due_date = 6.months.from_now
+        expect(project).to be_on_schedule
+      end
+      
+  end
+## End: estimates
+end
